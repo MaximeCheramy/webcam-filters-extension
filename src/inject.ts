7fd2c63b5b28dd581518a203c8a2c8fda74a54d6
Maxime Chéramy <maxime@cheramy.org>
2022-03-26 14:11:27 +0100

    fix: use webextension polyfill

--- src/inject.ts
+++ src/inject.ts
@@ -1,3 +1,5 @@
+import browser from 'webextension-polyfill'
+
 const script = document.createElement('script')
-script.src = chrome.runtime.getURL('build/main.js')
+script.src = browser.runtime.getURL('build/main.js')
 ;(document.head || document.documentElement).appendChild(script)
