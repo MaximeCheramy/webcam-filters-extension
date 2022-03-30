f04ff9f4ef26f290202ae60ec356be196f3f036e
Maxime Chéramy <maxime@cheramy.org>
2022-03-31 00:16:29 +0200

    chore: use esbuild watch mode for dev

--- esbuild.js
+++ esbuild.js
@@ -1,18 +1,23 @@
-const esbuild = require("esbuild");
+const esbuild = require('esbuild')
 
 esbuild
   .build({
-    entryPoints: [
-      "./src/main.ts",
-      "./src/monkey-patch.ts",
-      "./src/inject.ts"
-    ],
+    entryPoints: ['./src/main.ts', './src/monkey-patch.ts', './src/inject.ts'],
     bundle: true,
-    minify: process.env.NODE_ENV === "production",
-    target: ["chrome58", "firefox57"],
-    outdir: "./public/build",
+    minify: process.env.NODE_ENV === 'production',
+    target: ['chrome58', 'firefox57'],
+    outdir: './public/build',
+    watch:
+      process.env.NODE_ENV === 'dev'
+        ? {
+            onRebuild(error, result) {
+              if (error) console.error('Build failed')
+              else console.log('Build succeeded')
+            }
+          }
+        : false,
     define: {
-      "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`
+      'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
     }
   })
-  .catch(() => process.exit(1));
+  .catch(() => process.exit(1))
